c5289c6faeca5619accfc26c7d46628b7506f0c2
makiroggers <makiroggers@gmail.com>
2017-11-13 15:20:18 -0800

    Added power-ups and score HUD.

--- Unity/2D Infinite Runner/Assets/Scripts/CameraRunnerScript.cs
+++ Unity/2D Infinite Runner/Assets/Scripts/CameraRunnerScript.cs
@@ -7,7 +7,8 @@ public class CameraRunnerScript : MonoBehaviour {
 	public Transform player;
 	
 	// Update is called once per frame
-	void Update () {
+	void Update() 
+	{
 		transform.position = new Vector3(player.position.x + 6, 0, -10);
 	}
 }
--- Unity/2D Infinite Runner/Assets/Scripts/SpawnScript.cs
+++ Unity/2D Infinite Runner/Assets/Scripts/SpawnScript.cs
@@ -8,11 +8,13 @@ public class SpawnScript : MonoBehaviour {
 	public float spawnMin = 1f;
 	public float spawnMax = 2f;
 
-	void Start () {
+	void Start() 
+	{
 		Spawn();
 	}
 
-	void Spawn() {
+	void Spawn() 
+	{
 		Instantiate(obj[Random.Range (0, obj.GetLength(0))], transform.position, Quaternion.identity);
 		Invoke ("Spawn", Random.Range (spawnMin, spawnMax));
 	}
